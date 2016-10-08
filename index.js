d296547ea4e568ba5a60a5e282afa676ca5f6040
Valerii Iatsko <dwr@codingbox.io>
2016-10-09 00:42:11 +0200

    package.json & LICENSE

--- index.js
+++ index.js
@@ -14,9 +14,34 @@ function log(inputString) {
 // Some consts
 const workingDir = process.cwd();
 const templateDir = path.resolve(__dirname, 'template');
+const newJsAppBanner = chalk.black.bgBlue(' new-js-app ');
 
 // Welcome screen
-log(`${chalk.black.bgBlue(' new-js-app ')} Easy way to boostrap javascript apps.\n`);
+log(`${newJsAppBanner}\n`);
+log(`${newJsAppBanner} Easy way to boostrap javascript apps.\n`);
+log(`${newJsAppBanner}\n`);
+
+// Trying to fetch some information about user
+let gitUserName;
+try {
+  gitUserName = execSync('git config --get user.name', { encoding: 'utf8' }).trim();
+
+  log(`${newJsAppBanner} git user.name = ${gitUserName}\n`);
+} catch (e) {
+  // Ignored
+}
+
+let gitUserEmail;
+try {
+  gitUserEmail = execSync('git config --get user.email', { encoding: 'utf8' }).trim();
+  log(`${newJsAppBanner} git user.email = ${gitUserEmail}\n`);
+} catch (e) {
+  // Ignored
+}
+
+if (gitUserName || gitUserEmail) {
+  log(`${newJsAppBanner}\n`);
+}
 
 // Checking for the dir argument
 let dirToCreate;
@@ -42,7 +67,7 @@ try {
 // Creating directory for the new project
 fs.mkdirSync(dirToCreatePath);
 
-log(`${chalk.black.bgBlue(' new-js-app ')} ${chalk.green('Project directory created.')}\n`);
+log(`${newJsAppBanner} ${chalk.green('Project directory created.')}\n`);
 
 // Template files
 const templateFiles = fs.readdirSync(templateDir);
@@ -53,16 +78,51 @@ for (const templateFile of templateFiles) {
 
   fs.writeFileSync(destTo, fs.readFileSync(destFrom));
 
-  log(`${chalk.black.bgBlue(' new-js-app ')} ${chalk.green(`Creating ${templateFile}... OK`)}\n`);
+  log(`${newJsAppBanner} ${chalk.green(`Creating ${templateFile}... OK`)}\n`);
 }
 
+// Replacing some stuff in package.json
+const packageJsonPath = path.join(templateDir, 'package.json');
+
+const packageJson = JSON.parse(fs.readFileSync(packageJsonPath));
+
+packageJson.name = dirToCreate;
+packageJson.author = `${gitUserName} <${gitUserEmail}>`;
+
+fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+
+log(`${newJsAppBanner} ${chalk.green('Updating package.json... OK')}\n`);
+
+// Replacing author name in LICENSE file
+const licensePath = path.join(templateDir, 'LICENSE');
+
+fs.writeFileSync(licensePath,
+  fs.readFileSync(licensePath, { encoding: 'utf8' }).replace(/##AUTHOR##/, gitUserName)
+);
+
+log(`${newJsAppBanner} ${chalk.green('Updating LICENSE... OK')}\n`);
+
+// Running jest installation
+log(`${newJsAppBanner} ${chalk.green('Installing jest...')}\n`);
+execSync(
+  'npm install jest-cli --save-dev', {
+    cwd: dirToCreatePath,
+  });
+log(`${newJsAppBanner} ${chalk.green('Finished installing jest!')}\n`);
+
 // Running eslint installation
-log(`${chalk.black.bgBlue(' new-js-app ')} ${chalk.green('Installing eslint config...')}\n`);
+log(`${newJsAppBanner} ${chalk.green('Installing eslint config...')}\n`);
 execSync(
   'npm info eslint-config-airbnb peerDependencies --json | ' +
   'command sed \'s/[\{\},]//g ; s/: /@/g\' | xargs npm install --save-dev eslint-config-airbnb', { // eslint-disable-line
     cwd: dirToCreatePath,
   });
-log(`${chalk.black.bgBlue(' new-js-app ')} ${chalk.green('Finished installing eslint config!')}\n`);
+log(`${newJsAppBanner} ${chalk.green('Finished installing eslint config!')}\n`);
+
+// Finished
+
+log(`${newJsAppBanner}\n`);
+log(`${newJsAppBanner} Your app created. Happy coding!\n`);
+log(`${newJsAppBanner}\n`);
 
 log('\n');
